feaaaf2ad8d2a14c52fe2f6d3b2cfea0e50cf7ba
alisilsan <166475772+alisilsan@users.noreply.github.com>
2026-09-10 16:01:39 +0900

    Add files via upload

---
+++ items.js
@@ -0,0 +1,10 @@
+// 소비성 아이템 데이터
+
+const CONSUMABLE_ITEMS = {
+    pizza: { name: '하와이안 피자', color: '#ffb84d' },
+    lightning: { name: '번개의 룬', color: '#f4d35e' },
+    ruin: { name: '파멸의 룬', color: '#c65eff' },
+    cubeWatch: { name: '큐브 워치', color: '#79dbff' },
+    greatPill: { name: '대환단', color: '#ff8e70' },
+    seleneTear: { name: '셀레네의 눈물', color: '#bda6ff' },
+  };
